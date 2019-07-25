5005e7d7b1abd26564bb1aee61f53a9ecb30e1f4
Oshan Kottege <Oshan.Kottege@myob.com>
2019-07-25 17:28:14 +1200

    Started adding logic for getting all employees.

--- src/api/taskService.js
+++ src/api/taskService.js
@@ -1,9 +1,14 @@
-import axios from 'axios';
-import {baseUrl} from './apiUtils';
+import axios from "axios";
+import { baseUrl } from "./apiUtils";
 
 const apiBaseUrl = `${baseUrl}/tasks`;
 
-export default function getTasks() {
-  axios.get(apiBaseUrl)
-    .then(response => response.data);
+export function getTasks() {
+  axios.get(apiBaseUrl).then(response => response.data);
+}
+export function getAllEmployees() {
+  axios
+    .get(`${baseUrl}/employees`)
+    .then(response => response.data)
+    .catch(throw new Error("There was an error retrieving employees"));
 }
--- src/redux/actions/taskActions.js
+++ src/redux/actions/taskActions.js
@@ -1,18 +1,22 @@
-import * as types from './actionTypes';
-import getTasks from '../../api/taskService';
+import * as types from "./actionTypes";
+import getTasks from "../../api/taskService";
 
 export function createTask(task) {
-  return {type: types.TASK_CREATE, task};
+  return { type: types.TASK_CREATE, task };
 }
 
 export function loadTasksSuccess(tasks) {
-  return {type: types.TASKS_LOAD_SUCCESSUL, tasks};
+  return { type: types.TASKS_LOAD_SUCCESSUL, tasks };
 }
 
 export function loadTasks() {
   return dispatch => {
     getTasks()
       .then(tasks => dispatch(loadTasksSuccess(tasks)))
-      .catch(err => console.log('Error fetching tasks: ', err));
+      .catch(err => console.log("Error fetching tasks: ", err));
   };
+}
+
+export function loadEmployees() {
+  return dispatch => {};
 }
